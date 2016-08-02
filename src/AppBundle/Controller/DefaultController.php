9fb6b63de9cf7af8ff3dd41944c066ea351369dd
Emeka U Echeruo <emeka.echeruo@gmail.com>
2016-08-02 12:51:22 -0700

    Adding initial setup for symfony

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,301 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use AppBundle\Entity\Task;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+        ));
+    }
+	
+	public function ajaxViewAllAction(Request $request) {
+		
+        if (!$request->isXmlHttpRequest()) {
+            throw new NotFoundHttpException();
+        }
+		
+		$response = array('status' => 'failure', 'message' => 'Could not find Record');
+		$er = $this->getDoctrine()->getManager()->getRepository('AppBundle:Task')
+				   ->createQueryBuilder('t')
+				   ->where('t.deleted = :deleted')
+				   ->setParameter('deleted', false)
+					->addOrderBy('t.dateCreated', 'ASC');
+					
+		$tasks = $er->getQuery()->getResult();
+		if($tasks) {
+			$response = array('status' => 'success');		
+			foreach($tasks as $task) {
+				$response[] = array('id' => $task->getId(), 'title' => $task->getTitle(), 'note' => $task->getNote(), 'completed' => $task->getCompleted(), 'deleted' => $task->getDeleted());
+			}
+			$response['count'] = count($tasks);
+			return new JsonResponse($response, 200);
+		}
+		return new JsonResponse($response, 400);
+	}
+
+	public function ajaxViewCompleteAction(Request $request) {
+		
+        if (!$request->isXmlHttpRequest()) {
+            throw new NotFoundHttpException();
+        }
+		
+		$response = array('status' => 'failure', 'message' => 'Could not find Record');
+		$er = $this->getDoctrine()->getManager()->getRepository('AppBundle:Task')
+				   ->createQueryBuilder('t')
+				   ->where('t.completed = :completed')
+				   ->setParameter('completed', true)
+				   ->andWhere('t.deleted = :deleted')
+				   ->setParameter('deleted', false)
+				   ->addOrderBy('t.dateCreated', 'ASC');
+					
+		$tasks = $er->getQuery()->getResult();
+		if($tasks) {
+			$response = array('status' => 'success');		
+			foreach($tasks as $task) {
+				$response[] = array('id' => $task->getId(), 'title' => $task->getTitle(), 'note' => $task->getNote(), 'completed' => $task->getCompleted(), 'deleted' => $task->getDeleted());
+			}
+			$response['count'] = count($tasks);
+			return new JsonResponse($response, 200);
+		}
+		return new JsonResponse($response, 400);
+	}
+
+	public function ajaxViewActiveAction(Request $request) {
+		
+        if (!$request->isXmlHttpRequest()) {
+            throw new NotFoundHttpException();
+        }
+		
+		$response = array('status' => 'failure', 'message' => 'Could not find Record');
+		$er = $this->getDoctrine()->getManager()->getRepository('AppBundle:Task')
+				   ->createQueryBuilder('t')
+				   ->where('t.completed = :completed')
+				   ->setParameter('completed', false)
+				   ->andWhere('t.deleted = :deleted')
+				   ->setParameter('deleted', false)
+				   ->addOrderBy('t.dateCreated', 'ASC');
+					
+		$tasks = $er->getQuery()->getResult();
+		if($tasks) {
+			$response = array('status' => 'success');		
+			foreach($tasks as $task) {
+				$response[] = array('id' => $task->getId(), 'title' => $task->getTitle(), 'note' => $task->getNote(), 'completed' => $task->getCompleted(), 'deleted' => $task->getDeleted());
+			}
+			$response['count'] = count($tasks);
+			return new JsonResponse($response, 200);
+		}
+		return new JsonResponse($response, 400);
+	}
+	
+	public function ajaxViewAction(Request $request) {
+		
+        if (!$request->isXmlHttpRequest()) {
+            throw new NotFoundHttpException();
+        }
+		
+		$id = $request->request->get('id');
+		$response = array('status' => 'failure', 'message' => 'Could not find Record');
+		if(is_numeric($id)) {
+			$er = $this->getDoctrine()->getManager()->getRepository('AppBundle:Task')
+					   ->createQueryBuilder('t')
+					   ->where('t.id = :taskId')
+					   ->setParameter('taskId', $id)
+					   ->andWhere('t.deleted = :deleted')
+					   ->setParameter('deleted', false);
+					   
+			$task = $er->getQuery()->getResult();
+			if($task) {
+				$response = array('status' => 'success', 'id' => $task[0]->getId(), 'title' => $task[0]->getTitle(), 'note' => $task[0]->getNote(), 'completed' => $task[0]->getCompleted(), 'deleted' => $task[0]->getDeleted());
+				return new JsonResponse($response, 200);
+			}
+		}
+		return new JsonResponse($response, 400);
+	}
+
+	public function ajaxCreateAction(Request $request) {
+		
+        if (!$request->isXmlHttpRequest()) {
+            throw new NotFoundHttpException();
+        }
+		
+		$title = $request->request->get('title');
+		$response = array('status' => 'failure', 'message' => 'Could not find Record');
+		if($title) {
+			$response['message'] = "Please enter your Title";
+			if($title) {
+				$task = new Task();
+				$task->setTitle($title);
+				$er = $this->container->get('doctrine')->getEntityManager();
+				$er->persist($task);
+				$er->flush();	
+				
+				$response = array('status' => 'success');
+				return new JsonResponse($response, 200);
+			}
+		}
+		return new JsonResponse($response, 400);
+	}
+	
+	public function ajaxUpdateAction(Request $request) {
+		
+        if (!$request->isXmlHttpRequest()) {
+            throw new NotFoundHttpException();
+        }
+		
+		$id = $request->request->get('id');
+		$response = array('status' => 'failure', 'message' => 'Could not find Record');
+		if(is_numeric($id)) {
+			$title = $request->request->get('title');
+			$note = $request->request->get('note', '');
+			$response['message'] = "Please enter your Title";
+			if($title) {
+				$er = $this->getDoctrine()->getManager()->getRepository('AppBundle:Task')
+						   ->createQueryBuilder('t')
+						   ->update()
+						   ->set('t.title', ':title')
+						   ->setParameter('title', $title)
+						   ->set('t.lastModified', ':lastModified')
+						   ->setParameter('lastModified', new \DateTime())
+						   ->where('t.id = :taskId')
+						   ->setParameter('taskId', $id)
+						   ->andWhere('t.deleted = :deleted')
+						   ->setParameter('deleted', false);
+						   
+				$task = $er->getQuery()->getResult();
+				if($task) {
+					$response = array('status' => 'success');
+					return new JsonResponse($response, 200);
+				}
+			}
+		}
+		return new JsonResponse($response, 400);
+	}
+	
+	public function ajaxRemoveAction(Request $request) {
+		
+        if (!$request->isXmlHttpRequest()) {
+            throw new NotFoundHttpException();
+        }
+		
+		$id = $request->request->get('id');
+		$response = array('status' => 'failure', 'message' => 'Could not find Record');
+		if(is_numeric($id)) {
+			$er = $this->getDoctrine()->getManager()->getRepository('AppBundle:Task')
+					   ->createQueryBuilder('t')
+					   ->update()
+					   ->set('t.deleted', ':deleted')
+					   ->setParameter('deleted', true)
+					   ->set('t.lastModified', ':lastModified')
+					   ->setParameter('lastModified', new \DateTime())
+					   ->where('t.id = :taskId')
+					   ->setParameter('taskId', $id);					   
+					   
+			$task = $er->getQuery()->getResult();
+			if($task) {
+				$response = array('status' => 'success');
+				return new JsonResponse($response, 200);
+			}
+		}
+		return new JsonResponse($response, 400);
+	}
+
+	public function ajaxRestoreAction(Request $request) {
+		
+        if (!$request->isXmlHttpRequest()) {
+            throw new NotFoundHttpException();
+        }
+		
+		$id = $request->request->get('id');
+		$response = array('status' => 'failure', 'message' => 'Could not find Record');
+		if(is_numeric($id)) {
+			$er = $this->getDoctrine()->getManager()->getRepository('AppBundle:Task')
+					   ->createQueryBuilder('t')
+					   ->update()
+					   ->set('t.deleted', ':deleted')
+					   ->setParameter('deleted', false)
+					   ->where('t.id = :taskId')
+					   ->setParameter('taskId', $id)
+					   ->andWhere('t.deleted = :deleted')
+					   ->setParameter('deleted', true);
+			$task = $er->getQuery()->getResult();
+			if($task) {
+				return ajaxViewAllAction($request);
+			}
+		}
+		return new JsonResponse($response, 400);
+	}
+	
+	public function ajaxActiveAction(Request $request) {
+		
+        if (!$request->isXmlHttpRequest()) {
+            throw new NotFoundHttpException();
+        }
+		
+		$id = $request->request->get('id');
+		$response = array('status' => 'failure', 'message' => 'Could not find Record');
+		if(is_numeric($id)) {
+			$er = $this->getDoctrine()->getManager()->getRepository('AppBundle:Task')
+					   ->createQueryBuilder('t')
+					   ->update()
+					   ->set('t.completed', ':completed')
+					   ->setParameter('completed', false)
+					   ->set('t.lastModified', ':lastModified')
+					   ->setParameter('lastModified', new \DateTime())
+					   ->where('t.id = :taskId')
+					   ->setParameter('taskId', $id)
+					   ->andWhere('t.deleted = :deleted')
+					   ->setParameter('deleted', false);
+			$task = $er->getQuery()->getResult();
+			if($task) {
+				$response = array('status' => 'success');
+				return new JsonResponse($response, 200);
+			}
+		}
+		return new JsonResponse($response, 400);
+	}
+
+	public function ajaxCompleteAction(Request $request) {
+		
+        if (!$request->isXmlHttpRequest()) {
+            throw new NotFoundHttpException();
+        }
+		
+		$id = $request->request->get('id');
+		$response = array('status' => 'failure', 'message' => 'Could not find Record');
+		if(is_numeric($id)) {
+			$er = $this->getDoctrine()->getManager()->getRepository('AppBundle:Task')
+					   ->createQueryBuilder('t')
+					   ->update()
+					   ->set('t.completed', ':completed')
+					   ->setParameter('completed', true)
+					   ->set('t.lastModified', ':lastModified')
+					   ->setParameter('lastModified', new \DateTime())
+					   ->where('t.id = :taskId')
+					   ->setParameter('taskId', $id)
+					   ->andWhere('t.deleted = :deleted')
+					   ->setParameter('deleted', false);
+			$task = $er->getQuery()->getResult();
+			if($task) {
+				$response = array('status' => 'success');
+				return new JsonResponse($response, 200);
+			}
+		}
+		return new JsonResponse($response, 400);
+	}
+	
+}
---
+++ src/AppBundle/Entity/Task.php
@@ -0,0 +1,224 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Task
+ *
+ * @ORM\Table(name="task")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\TaskRepository")
+ */
+class Task
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=50)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="note", type="text", nullable=true)
+     */
+    private $note;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="completed", type="boolean")
+     */
+    private $completed;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="deleted", type="boolean")
+     */
+    private $deleted;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="lastModified", type="datetime")
+     */
+    private $lastModified;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dateCreated", type="datetime")
+     */
+    private $dateCreated;
+
+    public function __construct()
+    {
+		$datetime = new \DateTime();
+		$this->title = '';
+		$this->note = '';
+		$this->completed = false;
+		$this->deleted = false;
+		$this->lastModified = $datetime;
+		$this->dateCreated = $datetime;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Task
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set note
+     *
+     * @param string $note
+     * @return Task
+     */
+    public function setNote($note)
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    /**
+     * Get note
+     *
+     * @return string 
+     */
+    public function getNote()
+    {
+        return $this->note;
+    }
+
+    /**
+     * Set completed
+     *
+     * @param boolean $completed
+     * @return Task
+     */
+    public function setCompleted($completed)
+    {
+        $this->completed = $completed;
+
+        return $this;
+    }
+
+    /**
+     * Get completed
+     *
+     * @return boolean 
+     */
+    public function getCompleted()
+    {
+        return $this->completed;
+    }
+
+    /**
+     * Set deleted
+     *
+     * @param boolean $deleted
+     * @return Task
+     */
+    public function setDeleted($deleted)
+    {
+        $this->deleted = $deleted;
+
+        return $this;
+    }
+
+    /**
+     * Get deleted
+     *
+     * @return boolean 
+     */
+    public function getDeleted()
+    {
+        return $this->deleted;
+    }
+
+    /**
+     * Set lastModified
+     *
+     * @param \DateTime $lastModified
+     * @return Task
+     */
+    public function setLastModified($lastModified)
+    {
+        $this->lastModified = $lastModified;
+
+        return $this;
+    }
+
+    /**
+     * Get lastModified
+     *
+     * @return \DateTime 
+     */
+    public function getLastModified()
+    {
+        return $this->lastModified;
+    }
+
+    /**
+     * Set dateCreated
+     *
+     * @param \DateTime $dateCreated
+     * @return Task
+     */
+    public function setDateCreated($dateCreated)
+    {
+        $this->dateCreated = $dateCreated;
+
+        return $this;
+    }
+
+    /**
+     * Get dateCreated
+     *
+     * @return \DateTime 
+     */
+    public function getDateCreated()
+    {
+        return $this->dateCreated;
+    }
+}
